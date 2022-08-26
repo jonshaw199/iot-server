900005da4b790bb8ed9fc14168b1a910408167dd
Jon <jonshaw199@gmail.com>
2022-08-26 11:50:24 -0700

    Adding remote route; updating websocket route path

--- app.ts
+++ app.ts
@@ -3,15 +3,20 @@ var express = require("express");
 const app = express();
 var expressWs = require("express-ws")(app);
 
-const port = 3000;
-
-app.ws("/", (ws, req) => {
+app.ws("/ws", (ws, req) => {
   ws.on("message", (msg) => {
     console.log(msg);
     ws.send(`fluck you`);
   });
 });
+const rootWss = expressWs.getWss("/");
 
+app.post("/remote", (req, res) => {
+  res.send("Broadcasting message...");
+  rootWss?.clients?.forEach((client) => client.send("Heyo"));
+});
+
+const port = 3000;
 app.listen(port, () => {
   console.log(`AF1 server is running on port ${port}.`);
 });
