aa9cd4a183df106e2137bc56d24284cd9126859b
Jon <jonshaw199@gmail.com>
2022-08-25 18:35:47 -0700

    Using express-ws package

--- app.ts
+++ app.ts
@@ -1,8 +1,17 @@
-import express from "express";
+var express = require("express");
 
 const app = express();
+var expressWs = require("express-ws")(app);
+
 const port = 3000;
 
+app.ws("/", (ws, req) => {
+  ws.on("message", (msg) => {
+    console.log(msg);
+    ws.send(`fluck you`);
+  });
+});
+
 app.listen(port, () => {
-  console.log(`Timezones by location application is running on port ${port}.`);
+  console.log(`AF1 server is running on port ${port}.`);
 });
