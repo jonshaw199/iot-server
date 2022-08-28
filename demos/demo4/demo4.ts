1c44b3da82f4ee0e907db923e6ba93b10497cb76
Jon <jonshaw199@gmail.com>
2022-08-28 15:34:16 -0700

    Adding color changing to demo4

--- demos/demo4/demo4.ts
+++ demos/demo4/demo4.ts
@@ -2,7 +2,8 @@ import { clearDriftless, setDriftlessInterval } from "driftless";
 
 const MAX_BRIGHTNESS = 200;
 const SCENE_MS = 7000;
-const INTERVAL_MS = 100;
+const BRIGHTNESS_INTERVAL_MS = 50;
+const COLOR_INTERVAL_MS = 60000;
 
 let coefs = [
   ...Array(MAX_BRIGHTNESS / 10).fill(0),
@@ -12,9 +13,28 @@ let coefs = [
 coefs = [...coefs, ...coefs.reverse()];
 
 export default class Demo4 {
-  private static intervalId: number = -1;
+  private static brightnessIntervalId: number = -1;
+  private static colorIntervalId: number = -1;
   private static startMs: number;
   private static clients: any[];
+  private static rgb = { r: 255, g: 0, b: 0 };
+
+  static updateRGB() {
+    if (Demo4.rgb.r) {
+      Demo4.rgb.g = 255;
+      Demo4.rgb.r = 0;
+    } else if (Demo4.rgb.g) {
+      Demo4.rgb.b = 255;
+      Demo4.rgb.g = 0;
+    } else {
+      Demo4.rgb.r = 255;
+      Demo4.rgb.b = 0;
+    }
+  }
+
+  static getRGB() {
+    return [Demo4.rgb.r, Demo4.rgb.g, Demo4.rgb.b];
+  }
 
   static demo() {
     const curMs = new Date().getTime();
@@ -25,34 +45,46 @@ export default class Demo4 {
 
     const curArrIdx = Math.floor(curSceneRatio * coefs.length);
     const newBrightness = coefs[curArrIdx];
+    const [red, green, blue] = Demo4.getRGB();
 
     const msg = {
-      brightness: newBrightness,
-      color: "red",
       state: 3, // Demo4
       senderID: -1, // Server
-      type: 4, // TYPE_RUN_DATA
+      type: 4, // TYPE_RUN_DATA,
+      brightness: newBrightness,
+      red,
+      green,
+      blue,
     };
     console.log(`Sending msg: ${JSON.stringify(msg)}`);
     (Demo4.clients || []).forEach((client) => client.send(JSON.stringify(msg)));
   }
 
   static init(c: any[]) {
-    this.startMs = new Date().getTime();
-    this.intervalId = setDriftlessInterval(this.demo, INTERVAL_MS);
-    this.clients = c;
+    Demo4.startMs = new Date().getTime();
+    Demo4.brightnessIntervalId = setDriftlessInterval(
+      Demo4.demo,
+      BRIGHTNESS_INTERVAL_MS
+    );
+    Demo4.colorIntervalId = setDriftlessInterval(
+      Demo4.updateRGB,
+      COLOR_INTERVAL_MS
+    );
+    Demo4.clients = c;
   }
 
   static deinit() {
-    clearDriftless(this.intervalId);
-    this.intervalId = -1;
+    clearDriftless(Demo4.brightnessIntervalId);
+    clearDriftless(Demo4.colorIntervalId);
+    Demo4.brightnessIntervalId = -1;
+    Demo4.colorIntervalId = -1;
   }
 
   static toggle(c: any[]) {
-    if (this.intervalId < 0) {
-      this.init(c);
+    if (Demo4.brightnessIntervalId < 0) {
+      Demo4.init(c);
     } else {
-      this.deinit();
+      Demo4.deinit();
     }
   }
 }
