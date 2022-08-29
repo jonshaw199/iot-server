7d934edcddb300b5335793b2232273e226e87262
Jon <jonshaw199@gmail.com>
2022-08-29 07:54:47 -0700

    Temp bugfix

--- app.ts
+++ app.ts
@@ -13,6 +13,13 @@ enum MessageType {
   TYPE_RUN_DATA = 4,
 }
 
+enum State {
+  STATE_DEMO1 = 0,
+  STATE_DEMO2 = 1,
+  STATE_DEMO3 = 2,
+  STATE_DEMO4 = 3,
+}
+
 app.use(express.json());
 
 app.ws("/ws", (ws, req: Request) => {
@@ -31,10 +38,14 @@ app.post("/remote", (req: Request, res: Response) => {
       senderID: -1,
     });
     (rootWss.clients || []).forEach((client) => client.send(msg));
+
+    if (req.body.state === State.STATE_DEMO4) {
+      demo4.toggle(rootWss.clients);
+    }
+
     return res.send("State change success");
   }
-  demo4.toggle(rootWss.clients);
-  res.send();
+  res.send("Fail");
 });
 
 const port = 3000;
