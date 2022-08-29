6cdb745f96271972140d8fcb617f848ea1d13ed3
Jon <jonshaw199@gmail.com>
2022-08-28 17:03:19 -0700

    Handling state change requests in /remote

--- app.ts
+++ app.ts
@@ -3,8 +3,19 @@ const app = express();
 var expressWs = require("express-ws")(app);
 
 import demo4 from "./demos/demo4/demo4";
+import { Request, Response } from "express";
 
-app.ws("/ws", (ws, req) => {
+enum MessageType {
+  TYPE_NONE = 0,
+  TYPE_HANDSHAKE_REQUEST = 1,
+  TYPE_HANDSHAKE_RESPONSE = 2,
+  TYPE_CHANGE_STATE = 3,
+  TYPE_RUN_DATA = 4,
+}
+
+app.use(express.json());
+
+app.ws("/ws", (ws, req: Request) => {
   ws.on("message", (msg) => {
     console.log(msg);
     ws.send(`Message received`);
@@ -12,7 +23,16 @@ app.ws("/ws", (ws, req) => {
 });
 const rootWss = expressWs.getWss("/");
 
-app.post("/remote", (req, res) => {
+app.post("/remote", (req: Request, res: Response) => {
+  if (req.body.type === MessageType.TYPE_CHANGE_STATE) {
+    const msg = JSON.stringify({
+      type: MessageType.TYPE_CHANGE_STATE,
+      state: req.body.state,
+      senderID: -1,
+    });
+    (rootWss.clients || []).forEach((client) => client.send(msg));
+    return res.send("State change success");
+  }
   demo4.toggle(rootWss.clients);
   res.send();
 });
