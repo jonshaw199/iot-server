8f209db61f0aa3e8f043df9c9671bcec2b70a852
jon <jonshaw199@gmail.com>
2023-05-04 05:18:13 +0000

    Improve postCreate.sh

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,8 +1,12 @@
 sudo apk add npm git
 npm i
-git clone https://github.com/jonshaw199/crypt.git
-read -p "Decrypt .env files? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
-read -p "Enter encryption key: " key
-read -p "Enter initialization vector: " iv
-node crypt/index.js -d .. .env.encrypted "$key" "$iv"
+
+read -p "Decrypt .env files? (Y/N): " confirm
+if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
+    git clone https://github.com/jonshaw199/crypt.git
+    read -p "Enter encryption key: " key
+    read -p "Enter initialization vector: " iv
+    node crypt/index.js -d .. .env.encrypted "$key" "$iv"
+    rm -rf crypt
+fi
 echo "postCreate.sh complete"
