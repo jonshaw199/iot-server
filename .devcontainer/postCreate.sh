95b9b2115c88635d7837c5abe24bff6de1964ec2
jon <jonshaw199@gmail.com>
2023-05-03 22:03:45 -0700

    Add .env.encrypted and decrypt in postCreate.sh

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,4 +1,8 @@
 sudo apk add npm git
 npm i
 git clone https://github.com/jonshaw199/crypt.git
-mv crypt ../node_modules
+read -p "Decrypt .env files? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+read -p "Enter encryption key: " key
+read -p "Enter initialization vector: " iv
+node crypt/index.js -d .. .env.encrypted "$key" "$iv"
+echo "postCreate.sh complete"
